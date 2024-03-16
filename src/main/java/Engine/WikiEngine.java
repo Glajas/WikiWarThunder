ea3e2fbb589fb6db1557d664b4579be7838e55b3
Glajas <jas.glazowski@gmail.com>
2024-03-16 01:56:17 +0100

    Migrated to Maven

--- src/main/java/Engine/WikiEngine.java
+++ src/main/java/Engine/WikiEngine.java
@@ -1,11 +1,19 @@
 package Engine;
 
 import GUI.InterfaceGUI;
+import Utilities.StatsFrom3Days;
+
+import java.util.List;
 
 public class WikiEngine {
     private InterfaceGUI interfaceGUI;
+    private List<StatsFrom3Days> statList;
 
     public WikiEngine(InterfaceGUI interfaceGUI) {
         this.interfaceGUI = interfaceGUI;
+    }
+
+    public void getAllStats(String filepath) {
+        this.statList =
     }
 }
--- src/main/java/Utilities/CsvReader.java
+++ src/main/java/Utilities/CsvReader.java
@@ -5,6 +5,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class CsvReader {
     public String readFirstLine(String fileName) throws IOException {
@@ -16,5 +17,14 @@ public class CsvReader {
     public List<String> readFirstKLines(String fileName, int k) throws IOException {
         List<String> lines = Files.readAllLines(Path.of(fileName));
         return new ArrayList<>(lines.subList(1, Math.min(k + 1, lines.size())));
+    }
+
+    public List<StatsFrom3Days> readAllStatsByCountry(String fileName, String filterCountry) throws IOException {
+        List<String> lines = Files.readAllLines(Path.of(fileName));
+        return lines.stream()
+                .skip(1)
+                .map(StatsFrom3Days::new)
+                .filter(stats -> (filterCountry == null || filterCountry.isEmpty()) || stats.getNation().equalsIgnoreCase(filterCountry))
+                .collect(Collectors.toList());
     }
 }
